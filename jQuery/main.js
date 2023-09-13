6a1da86d4760d99ccb0d139b25507dc787ca1391
WILLIAMS DAMASCENO PINHEIRO FILHO <133906052+williamsdamasceno@users.noreply.github.com>
2023-09-13 13:17:41 -0300

    Update main.js

--- jQuery/main.js
+++ jQuery/main.js
@@ -10,11 +10,11 @@ $(document).ready(function() {
          $('form').on('submit', function(e) {
             e.preventDefault();
             const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
-            const novoItem = $('<li> style="display: none"</li>')
+            const novoItem = $('<li style="display: none"></li>')
             $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
             $(`
               <div class="overlay-imagem-link">
-                <a href="${enderecoDaNovaImagem}" target="_blank" tittle="Ver imagem em tamanho real"
+                <a href="${enderecoDaNovaImagem}" target="_blank" tittle="Ver imagem em tamanho real">
                         Ver imagem em tamanho real
                         </a>
              </div>
